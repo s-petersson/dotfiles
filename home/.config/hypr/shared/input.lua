04d03e696dd2502f0fa32efda0d5f75d95982f38
Simon Petersson <peterssonaren@gmail.com>
2026-08-30 20:08:24 +0200

    set default keyboard layout to US in hyprland

--- home/.config/hypr/shared/input.lua
+++ home/.config/hypr/shared/input.lua
@@ -1,6 +1,6 @@
 hl.config({
 	input = {
-		kb_layout = "se,us",
+		kb_layout = "us,se",
 		kb_options = "caps:escape,shift:both_capslock_cancel,grp:alts_toggle",
 		repeat_rate = 80,
 		repeat_delay = 250,
