f47206b1a9a8865ecaa83952dfc24ee064e48252
Simon Petersson <peterssonaren@gmail.com>
2026-08-30 12:48:43 +0200

    add herdr installation + documentation on package installation split

--- setup/pre/10-install-utilities.sh
+++ setup/pre/10-install-utilities.sh
@@ -2,6 +2,10 @@
 
 source "${DOTFILES_LIB:?}/packages.sh"
 
+# Install tools available through the platform package manager.
+# Keep this file limited to packages handled by lib/dotfiles/packages.sh
+# (Homebrew on macOS, pacman on Arch). Tools that require custom installers
+# belong in setup/pre/20-install-external-tools.sh instead.
 dotfiles_install_packages \
     curl \
     openssh \
